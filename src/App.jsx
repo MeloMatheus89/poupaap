a21af491a4a575d5869a481851bd8ab47ef05a15
MeloMatheus89 <156861214+MeloMatheus89@users.noreply.github.com>
2026-03-23 18:53:03 -0300

    Add Typography component and styles for text formatting

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,18 @@ import { Container } from "./components/Container/";
 import { Aside } from "./components/Container/Aside";
 import { Main } from "./components/Main";
 import { SearchInput } from "./components/SearchInput";
+import { Typography } from "./components/Typography";
 function App() {
   return (
     <Container>
       <Aside></Aside>
       <Main>
         <SearchInput></SearchInput>
+        <div>
+          <Typography variant="h1">Olá, Matheus!</Typography>
+          <Typography variant="body">Veja como estão susas finanças hoje.</Typography>
+        </div>
+        <div></div>
       </Main>
     </Container>
   );
