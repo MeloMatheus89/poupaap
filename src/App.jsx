4ec97522085cbb94014bcc5381d9be659df3f5ca
MeloMatheus89 <156861214+MeloMatheus89@users.noreply.github.com>
2026-03-31 13:10:13 -0300

    Criação dos cards. Tentativa minha de estilização de acordo com o Figma

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Aside } from "./components/Container/Aside";
 import { Main } from "./components/Main";
 import { SearchInput } from "./components/SearchInput";
 import { Typography } from "./components/Typography";
+import { Card } from "./components/Card";
 function App() {
   return (
     <Container>
@@ -13,7 +14,24 @@ function App() {
           <Typography variant="h1">Olá, Matheus!</Typography>
           <Typography variant="body">Veja como estão susas finanças hoje.</Typography>
         </div>
-        <div></div>
+        <section>
+          <Card>
+            <Card.Header>Orçamento diário disponível</Card.Header>
+            <Card.Body>R$ 200,00</Card.Body>
+          </Card>
+          <Card>
+            <Card.Header>Orçamento diário disponível</Card.Header>
+            <Card.Body>R$ 200,00</Card.Body>
+          </Card>
+          <Card>
+            <Card.Header>Orçamento diário disponível</Card.Header>
+            <Card.Body>R$ 200,00</Card.Body>
+          </Card>
+          <Card>
+            <Card.Header>Orçamento diário disponível</Card.Header>
+            <Card.Body>R$ 200,00</Card.Body>
+          </Card>
+        </section>
       </Main>
     </Container>
   );
