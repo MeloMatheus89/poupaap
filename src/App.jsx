f7e9c8a797d8cfa7789bd63d5e8b253fd13587be
MeloMatheus89 <156861214+MeloMatheus89@users.noreply.github.com>
2026-04-01 16:31:27 -0300

    feat: Add DailyBudget and SavingsStatus components with styles; update: Card component structure

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import { Aside } from "./components/Container/Aside";
 import { Main } from "./components/Main";
 import { SearchInput } from "./components/SearchInput";
 import { Typography } from "./components/Typography";
-import { Card } from "./components/Card";
+import Card from "./components/Card";
+import { DailyBudget } from "./components/DailyBudget";
+import { SavingsStatus } from "./components/SavingsStatus";
 function App() {
   return (
     <Container>
@@ -12,16 +14,20 @@ function App() {
         <SearchInput></SearchInput>
         <div>
           <Typography variant="h1">Olá, Matheus!</Typography>
-          <Typography variant="body">Veja como estão susas finanças hoje.</Typography>
+          <Typography variant="body">Veja como estão suas finanças hoje.</Typography>
         </div>
         <section>
           <Card>
             <Card.Header>Orçamento diário disponível</Card.Header>
-            <Card.Body>R$ 200,00</Card.Body>
+            <Card.Body>
+              <DailyBudget value={250} />
+            </Card.Body>
           </Card>
           <Card>
-            <Card.Header>Orçamento diário disponível</Card.Header>
-            <Card.Body>R$ 200,00</Card.Body>
+            <Card.Header>Progresso da meta financeira</Card.Header>
+            <Card.Body>
+              <SavingsStatus percent={40} />
+            </Card.Body>
           </Card>
           <Card>
             <Card.Header>Orçamento diário disponível</Card.Header>
